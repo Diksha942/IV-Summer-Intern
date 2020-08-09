77bd2f56d7e34d80c9315721ea1f25c65c631a87
Diksha942 <59208103+Diksha942@users.noreply.github.com>
2020-08-09 14:51:30 +0530

    Add files via upload

---
+++ Virtual_Drawing_Pad (colour detection).py
@@ -0,0 +1,126 @@
+import numpy as np
+import cv2 as cv
+from matplotlib import pyplot as plt
+
+def nothing(x):
+    pass
+
+#function to draw a rectangle wherw the stylus has to be placed
+def draw_rect(frame):
+    global x,y
+    row,col,_=frame.shape
+    y=np.uint32((col/2)-12)
+    x=np.uint32((row/2)-12)
+    cv.rectangle(frame,(y,x),(y+24,x+24),(0,255,0),1)
+    return(frame)
+
+#Window where it'll draw 
+cv.namedWindow('Drawing Board')
+
+cv.createTrackbar("R", "Drawing Board", 0, 255, nothing)
+cv.createTrackbar("G", "Drawing Board", 0, 255, nothing)
+cv.createTrackbar("B", "Drawing Board", 0, 255, nothing)
+cv.createTrackbar("Size", "Drawing Board", 3, 30, nothing)
+
+kernel = cv.getStructuringElement(cv.MORPH_ELLIPSE,(30,30))
+#kernel2 = np.ones((5,5))
+points = []
+hist_flag = 0
+cont_flag=0
+
+cap = cv.VideoCapture(0)
+if not cap.isOpened():
+    print('Cannot open camera')
+    exit()
+
+while True:
+    ret, frame = cap.read()
+    if not ret :
+        print("Can't recieve frame, try again later maybe...")
+        break
+    frame = cv.flip(frame,1)
+    hsv=cv.cvtColor(frame,cv.COLOR_BGR2HSV)
+
+    #Input the colour range if Enter is pressed
+    if cv.waitKey(10)&0xff==13:
+        roi=np.zeros([24,24,3], dtype=hsv.dtype)
+        roi[0:24,0:24]=hsv[x:x+24,y:y+24]
+        hist_flag=1
+    else:
+        frame=draw_rect(frame)
+        cv.imshow('Original',frame)
+
+    #compute histograms of the colour range input
+    if hist_flag==1:
+        
+        hist = cv.calcHist([roi],[0,1],None,[180,256],[0,180,0,256])
+        cv.normalize(hist, hist,0,255,cv.NORM_MINMAX)
+        dst = cv.calcBackProject([hsv], [0, 1], hist, [0, 180, 0, 256], 1)
+
+        #try clearing the noise
+        dst=cv.morphologyEx(dst,cv.MORPH_CLOSE,kernel)
+        #dst=cv.dilate(dst, kernel2,iterations=1)
+
+        #making the mask
+        ret, thresh = cv.threshold(dst,127, 255, cv.THRESH_BINARY)
+        thresh1 = cv.merge((thresh, thresh, thresh))
+
+        masked = cv.bitwise_and(frame,thresh1)
+        cnt,_ = cv.findContours(thresh, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
+        
+        #if contour is detected, move forward
+        if len(cnt)>0:
+
+            c = max(cnt, key = cv.contourArea)
+
+            #drawing a rectangle around
+            rect = cv.minAreaRect(c)
+            global cx,cy
+            [(cx,cy),(w,h),_] = rect
+            box = cv.boxPoints(rect)
+            box = np.int0(box)
+
+            cv.drawContours(masked,[box],0,(0,0,255),2)
+            cv.circle(masked,(int(cx),int(cy)),3,(255,0,0),-1)
+            cv.imshow('Original', masked)
+            
+            board = np.zeros(frame.shape, np.uint8)
+            board[:] = [255,255,255]
+            brd_flag=1
+
+            #trackbars to set colour and size of the pen
+            r = cv.getTrackbarPos("R", "Drawing Board")
+            g = cv.getTrackbarPos("G", "Drawing Board")
+            b = cv.getTrackbarPos("B", "Drawing Board")
+            sz = cv.getTrackbarPos("Size", "Drawing Board")
+
+        else:
+            continue
+        #works as a pointer
+        cv.circle(board,(int(cx),int(cy)),sz,(b,g,r),-1)
+        #stops drawing when pressed p
+        if cv.waitKey(40)&0xff==ord('p'):
+            points.append((0,0))
+        else:
+            points.append((int(cx), int(cy)))
+        for i in range(1,len(points)):
+            if points[i]==(0,0):
+                pass
+            else:
+                if points[i-1]==(0,0):
+                    pass
+                else:
+                    cv.line(board,points[i-1],points[i],(b,g,r),sz)
+                cv.imshow('Drawing Board', board)
+            
+    if cv.waitKey(1)&0xff == 27:
+        break
+    #clears the board when pressed c
+    if cv.waitKey(5)&0xff == ord('c'):
+        points[:]=[]
+        continue
+
+cap.release()
+cv.destroyAllWindows()
+
+
---
+++ Virtual_Drawing_Pad.py
@@ -0,0 +1,118 @@
+import numpy as np
+import cv2 as cv
+from matplotlib import pyplot as plt
+
+def nothing(x):
+    pass
+#Window for drawing, which has trackbars for changing colours
+cv.namedWindow('Drawing Board')
+
+cv.createTrackbar("R", "Drawing Board", 0, 255, nothing)
+cv.createTrackbar("G", "Drawing Board", 0, 255, nothing)
+cv.createTrackbar("B", "Drawing Board", 0, 255, nothing)
+cv.createTrackbar("Size", "Drawing Board", 3, 30, nothing)
+
+#The main window
+cv.namedWindow("Trackbars")
+#Trackbars to change the hsv upper and lower limits
+cv.createTrackbar("L - H", "Trackbars", 0, 179, nothing)
+cv.createTrackbar("L - S", "Trackbars", 0, 255, nothing)
+cv.createTrackbar("L - V", "Trackbars", 0, 255, nothing)
+cv.createTrackbar("U - H", "Trackbars", 179, 179, nothing)
+cv.createTrackbar("U - S", "Trackbars", 255, 255, nothing)
+cv.createTrackbar("U - V", "Trackbars", 255, 255, nothing)
+
+cap = cv.VideoCapture(0)
+if not cap.isOpened():
+    print('Cannot open camera')
+    exit()
+
+kernel = np.ones((3,3), dtype=np.uint8)
+global hold_flag
+points = []
+cont_flag=0
+
+while True:
+    ret, frame = cap.read()
+    if not ret :
+        print("Can't recieve frame, try again later maybe...")
+        break
+    frame = cv.flip(frame,1)
+
+    #Trackbars to get the range of stylus
+    l_h = cv.getTrackbarPos("L - H", "Trackbars")
+    l_s = cv.getTrackbarPos("L - S", "Trackbars")
+    l_v = cv.getTrackbarPos("L - V", "Trackbars")
+    u_h = cv.getTrackbarPos("U - H", "Trackbars")
+    u_s = cv.getTrackbarPos("U - S", "Trackbars")
+    u_v = cv.getTrackbarPos("U - V", "Trackbars")
+    
+    lower_col = np.array([l_h,l_s,l_v])
+    upper_col = np.array([u_h,u_s,u_v])
+    
+    #creating mask
+    hsv = cv.cvtColor(frame,cv.COLOR_BGR2HSV)
+    mask = cv.inRange(hsv, lower_col,  upper_col)
+    mask = cv.morphologyEx(mask, cv.MORPH_CLOSE, kernel)
+
+    masked_img = cv.bitwise_and(frame, frame, mask = mask)
+    cv.imshow('Trackbars',masked_img)
+
+    #Pressing enter to create contour
+    if cv.waitKey(10)&0xff==13 or cont_flag==1:
+        #creating the contour
+        cnt,_ = cv.findContours(mask, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
+        cont_flag=1
+
+        if len(cnt)>0:
+            c = max(cnt, key = cv.contourArea) #finding contour with max area
+            #drawing a rectangle around
+            rect = cv.minAreaRect(c)
+            global cx,cy
+            [(cx,cy),(w,h),_] = rect
+            box = cv.boxPoints(rect)
+            box = np.int0(box)
+            cv.drawContours(masked_img,[box],0,(0,0,255),2)
+            #circle showing the middle
+            cv.circle(masked_img,(int(cx),int(cy)),3,(255,0,0),-1)
+            cv.imshow('Trackbars', masked_img)
+            
+            #making a board to draw
+            board = np.zeros(frame.shape, np.uint8)
+            board[:] = [255,255,255]
+
+            r = cv.getTrackbarPos("R", "Drawing Board")
+            g = cv.getTrackbarPos("G", "Drawing Board")
+            b = cv.getTrackbarPos("B", "Drawing Board")
+            sz = cv.getTrackbarPos("Size", "Drawing Board")
+    else:
+        continue
+    
+    cv.circle(board,(int(cx),int(cy)),sz,(b,g,r),-1)
+    #Stops writing when 'p' is pressed
+    if cv.waitKey(40)&0xff==ord('p'):
+        points.append((0,0))
+    else:
+        points.append((int(cx), int(cy)))
+    #Draw for all the points
+    for i in range(1,len(points)):
+        if points[i]==(0,0):
+            pass
+        else:
+            if points[i-1]==(0,0):
+                pass
+            else:
+                cv.line(board,points[i-1],points[i],(b,g,r),sz)
+            cv.imshow('Drawing Board', board)
+            
+    if cv.waitKey(1)&0xff == 27:
+        break
+    #Clear the board when pressed 'c'
+    if cv.waitKey(5)&0xff == ord('c'):
+        points[:]=[]
+        continue
+
+cap.release()
+cv.destroyAllWindows()
+
+
